a8f3ddd09c061f62be8fa5d24338a37d83ac3568
Juan Manuel Camara Diaz <juanma.caaz@gmail.com>
2021-10-17 18:06:49 +0200

    Primer apartado, entendiando los datos

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-# code goes here
+import pandas as pd
+import numpy as np
+import seaborn as snb
+
+DATA_DIR = 'data/insurance.csv'
+
+def read_database(dir : str):
+    return pd.read_csv(dir, delimiter= ',')
+
+def run():
+    df = read_database(DATA_DIR)
+    print(df.head(5))
+
+if __name__ == '__main__':
+    run()
